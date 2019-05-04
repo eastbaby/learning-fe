ca22ff9e147000bf92c16fa880165f0e6813465f
eastbaby <little94cc@outlook.com>
2019-05-04 16:27:03 +0800

    feat:cssloader(css module,post css),styleloader

--- webpack-learn/app/main.js
+++ webpack-learn/app/main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {render} from 'react-dom';
 import Greeter from './Greeter';
+import './main.css'; // 使用import 导入css文件
 
 render(<Greeter />, document.getElementById('root'));
--- webpack-learn/webpack.config.js
+++ webpack-learn/webpack.config.js
@@ -20,6 +20,22 @@ module.exports = {
           loader: "babel-loader"
         },
         exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          }, {
+            loader: "css-loader",
+            options: {
+              modules: true, // 指定启用css modules
+              localIdentName: '[name]__[local]--[hash:base64:5]' // 指定css的类名格式
+            }
+          }, {
+            loader: "postcss-loader"
+          }
+        ]
       }
     ]
   }
